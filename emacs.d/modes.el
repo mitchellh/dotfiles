eb5e578ad4dd46fbf728c06e5b2f9f07f6323576
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2010-07-13 09:54:46 -0700

    Fixed JS2 indentation problems

--- emacs.d/modes.el
+++ emacs.d/modes.el
@@ -21,3 +21,49 @@
 ;; Customize
 (setq js2-basic-offset 2)
 (setq js2-cleanup-whitespace t)
+
+;; Custom indentation function since JS2 indenting is terrible.
+;; Uses js-mode's (espresso-mode) indentation semantics.
+;;
+;; Based on: http://mihai.bazon.net/projects/editing-javascript-with-emacs-js2-mode
+;; (Thanks!)
+(defun my-js2-indent-function ()
+  (interactive)
+  (save-restriction
+    (widen)
+    (let* ((inhibit-point-motion-hooks t)
+           (parse-status (save-excursion (syntax-ppss (point-at-bol))))
+           (offset (- (current-column) (current-indentation)))
+           (indentation (js--proper-indentation parse-status))
+           node)
+
+      (save-excursion
+
+        ;; I like to indent case and labels to half of the tab width
+        (back-to-indentation)
+        (if (looking-at "case\\s-")
+            (setq indentation (+ indentation (/ js-indent-level 2))))
+
+        ;; consecutive declarations in a var statement are nice if
+        ;; properly aligned, i.e:
+        ;;
+        ;; var foo = "bar",
+        ;;     bar = "foo";
+        (setq node (js2-node-at-point))
+        (when (and node
+                   (= js2-NAME (js2-node-type node))
+                   (= js2-VAR (js2-node-type (js2-node-parent node))))
+          (setq indentation (+ 4 indentation))))
+
+      (indent-line-to indentation)
+      (when (> offset 0) (forward-char offset)))))
+
+(defun my-js2-mode-hook ()
+  (set (make-local-variable 'indent-line-function) 'my-js2-indent-function)
+  (define-key js2-mode-map [(return)] 'newline-and-indent)
+  (define-key js2-mode-map [(backspace)] 'c-electric-backspace)
+  (define-key js2-mode-map [(control d)] 'c-electric-delete-forward)
+  (message "JS2 mode hook ran."))
+
+;; Add the hook so this is all loaded when JS2-mode is loaded
+(add-hook 'js2-mode-hook 'my-js2-mode-hook)
