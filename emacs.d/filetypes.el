bcf9cf0a96b62bb1e378c2b9e03a72c570f9e46a
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2010-07-24 12:49:16 -0700

    Gemspecs are ruby files

--- emacs.d/filetypes.el
+++ emacs.d/filetypes.el
@@ -3,10 +3,11 @@
 ;;-----------------------------------------------------------------
 ;; Ruby
 (add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.builder$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.builder$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Thorfile$" . ruby-mode))
 
