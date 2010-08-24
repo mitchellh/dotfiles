deaf29388b6c1960365abcb667303cfbe71d0eb7
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2010-08-23 18:31:58 -0700

    Very lame xmonad config. More to come soon

---
+++ xmonad/xmonad.hs
@@ -0,0 +1,21 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+main = do
+  xmproc <- spawnPipe "xmobar /home/mitchellh/.xmobarrc"
+  xmonad $ defaultConfig
+    { terminal = "urxvt"
+    , modMask = mod4Mask
+    , manageHook = manageDocks <+> manageHook defaultConfig
+    , layoutHook = avoidStruts $ layoutHook defaultConfig
+    , logHook = dynamicLogWithPP $ xmobarPP
+                { ppOutput = hPutStrLn xmproc
+                , ppTitle = xmobarColor "green" "" . shorten 50
+                }
+    } `additionalKeys`
+    [ ((mod4Mask .|. shiftMask, xK_z), spawn "gnome-screensaver-command --lock")
+    ]
