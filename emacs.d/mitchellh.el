16fc9d379f23b1f94142a725e5f43f5011a94e72
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2010-09-07 14:03:33 -0700

    Meh, don't use ERC

--- emacs.d/mitchellh.el
+++ emacs.d/mitchellh.el
@@ -6,15 +6,3 @@
     (progn (require 'color-theme-tango)
            (require 'color-theme-tangotango)
            (color-theme-tangotango)))
-
-;;-----------------------------------------------------------------
-;; ERC
-;;-----------------------------------------------------------------
-(defun my-erc-connect ()
-  (interactive)
-  (progn (erc :server "irc.freenode.net" :port 6667 :nick "mitchellh")
-         (erc :server "irc.citrusbyte.com" :port 6667 :nick "mitchellh" :password "4pk4cUyb7NL14L4moom9ledwVIoXfo1")))
-
-(setq erc-autojoin-channels-alist
-      '(("freenode.net" "#vagrant" "#chef" "#cbzd" "#rubinius")
-        ("citrusbyte.com" "#cb")))
