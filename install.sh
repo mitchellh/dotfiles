feb0812e94d509c5be02d5840d66858b90ee45f4
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2019-01-27 08:54:04 -0800

    modernize install script a bit, automatically install vim plugins

--- install.sh
+++ install.sh
@@ -9,32 +9,57 @@ pushd $(dirname ${BASH_SOURCE[0]}) > /dev/null
 SCRIPT_DIR=$(pwd)
 popd > /dev/null
 
-UNAME=$(uname)
+# names in this list won't be dot-prefixed
+skip_dot_prefix=(nvim)
 
+# contains checks if an array ($2) contains a given element ($1).
+contains() {
+  local e match="$1"
+  shift
+  for e; do [[ "$e" == "$match" ]] && return 0; done
+  return 1
+}
+
+# copy or symlink all the dotfiles
+UNAME=$(uname)
 for path in $SCRIPT_DIR/*; do
   name=$(basename $path)
-  if [ ! $name == "README.md" -a ! $name == "install.sh" ]; then
-    target="$name"
-    if [ ! $name == "nvim" ]; then
-        target=".$name"
-    fi
-    target="$HOME/$target"
+  case $name in
+    *.md|*.sh) continue;;
+  esac
 
-    if [ -h $target ]; then
-      rm $target
-    elif [ -d $target ]; then
-      rm -rf $target
-    fi
-
-    case $UNAME in
-        CYGWIN* | MINGW32*)
-            cp -R "$PWD/$name" "$target"
-            echo "Copied $PWD/$name to $target."
-            ;;
-        *)
-            ln -s $path "$target"
-            echo "Linked $path to $target."
-            ;;
-    esac
+  # If the file is in the skip dot list, then we don't add a dot
+  target="$name"
+  if ! contains "$name" "${skip_dot_prefix[@]}"; then
+    target=".$name"
   fi
+
+  # Build our complete path to the home directory
+  target="$HOME/$target"
+  if [ -h $target ]; then
+    rm $target
+  elif [ -d $target ]; then
+    rm -rf $target
+  fi
+
+  case $UNAME in
+    CYGWIN* | MINGW32*)
+      cp -R $path "$target"
+      echo "Copied $name to $target."
+      ;;
+    *)
+      ln -s $path "$target"
+      echo "Linked $name to $target."
+      ;;
+  esac
 done
+
+# setup the vim directory
+pushd $HOME >/dev/null
+if [ ! -d ".vim/bundle" ]; then
+    echo "Installing Vim plugins"
+    mkdir -p .vim .vim/bundle .vim/backup .vim/swap .vim/cache .vim/undo
+    curl -fLo .vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    TERM=dumb vim +PlugInstall +qall >vim.log 2>&1
+fi
+popd >/dev/null
