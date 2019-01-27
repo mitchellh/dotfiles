40ef9a7b53de7a998b522af4f0bc7e81fea5d7ed
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2019-01-27 09:38:14 -0800

    devserver: don't setup vim on entrypoint cause dotfiles does that

--- devserver/entrypoint.sh
+++ devserver/entrypoint.sh
@@ -14,23 +14,10 @@ if [ ! -d "~/code/dotfiles" ]; then
   git clone https://github.com/mitchellh/dotfiles.git
   cd
 
-  # Delete all previous dotfiles
-  find ~ -maxdepth 1 -type f -name '.[!.]*' -exec rm {} \;
-
   # Install our own
   cd ~/code/dotfiles
   ./install.sh
   cd
-fi
-
-#--------------------------------------------------------------------
-# Vim
-
-if [ ! -d ".vim/bundle" ]; then
-    mkdir -p .vim .vim/bundle .vim/backup .vim/swap .vim/cache .vim/undo
-    curl -fLo .vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    echo "Installing Vim plugins"
-    TERM=dumb vim +PlugInstall +qall >vim.log 2>&1
 fi
 
 #--------------------------------------------------------------------
@@ -57,6 +44,6 @@ sudo chmod 0666 /dev/fuse
 
 # Start Keybase
 export KEYBASE_NO_SQUIRREL=1
-run_keybase
+run_keybase >/dev/null
 
 /bin/bash
